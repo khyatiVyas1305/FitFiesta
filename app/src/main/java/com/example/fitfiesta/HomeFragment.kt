76381204b8f2b561d5d61c543dcd4207e6ff4c6c
khyatiVyas1305 <khyativyas1305@gmail.com>
2023-11-04 22:05:46 -0400

    Commit for Pull

--- app/src/main/java/com/example/fitfiesta/HomeFragment.kt
+++ app/src/main/java/com/example/fitfiesta/HomeFragment.kt
@@ -1,39 +1,45 @@
 package com.example.fitfiesta
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.RecyclerView
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
 import java.util.Calendar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     lateinit var randomTV: TextView
     lateinit var randomImg : ImageView
+//    lateinit var tvStepsTaken: TextView
+//    lateinit var stepsCounter: CircularProgressBar
+    private lateinit var sharedPreferences: SharedPreferences
+
+//
+//    private var sensorManager: SensorManager? =null
+//    private var running = false
+//    private var totalSteps = 0f
+//    private var previousTotalSteps = 0f
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -46,6 +52,8 @@ class HomeFragment : Fragment() {
 
         randomTV = view.findViewById(R.id.cardTextView)
         randomImg = view.findViewById(R.id.cardImage)
+//        tvStepsTaken = view.findViewById(R.id.stepsTaken)
+//        stepsCounter = view.findViewById(R.id.stepsCounter)
 
         val calendar = Calendar.getInstance()
         val day = calendar[Calendar.DAY_OF_WEEK]
@@ -81,28 +89,87 @@ class HomeFragment : Fragment() {
             }
         }
 
+        //STEPS COUNTER
+//        sensorManager = requireActivity().getSystemService(Context.SENSOR_SERVICE) as SensorManager
+//        loadData()
+//        resetData()
+
 
         return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+    //SENSOR MANAGE
+//    override fun onResume() {
+//        super.onResume()
+//        running = true
+//        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+//
+//        if(stepSensor == null){
+//            Toast.makeText(requireContext(),"No sensor found on this device", Toast.LENGTH_SHORT).show()
+//        }else{
+//            sensorManager?.registerListener(this,stepSensor,SensorManager.SENSOR_DELAY_UI)
+//        }
+//    }
+//
+//    override fun onSensorChanged(event: SensorEvent?) {
+//        if(running){
+//            totalSteps = event!!.values[0]
+//            val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
+//            tvStepsTaken.text = ("$currentSteps")
+//
+//            stepsCounter.apply {
+//                setProgressWithAnimation(currentSteps.toFloat())
+//            }
+//        }
+//    }
+//
+//    private fun saveData() {
+//        sharedPreferences = requireActivity().getSharedPreferences("mySharedPref",Context.MODE_PRIVATE)
+//        val editor = sharedPreferences.edit()
+//        editor.putFloat("key1",previousTotalSteps)
+//        editor.apply()
+//    }
+//
+//    private fun resetData(){
+//        tvStepsTaken.setOnClickListener {
+//            Toast.makeText(requireContext(),"Long press to reset the counter", Toast.LENGTH_SHORT).show()
+//        }
+//        tvStepsTaken.setOnLongClickListener{
+//            previousTotalSteps = totalSteps
+//            tvStepsTaken.text = 0.toString()
+//            saveData()
+//            true
+//        }
+//    }
+//
+//    private fun loadData(){
+//        val sharedPreferences = requireActivity().getSharedPreferences("mySharedPref", Context.MODE_PRIVATE)
+//        val savedNumber = sharedPreferences.getFloat("key1",0f)
+//        Log.d("HomeFragment", "$savedNumber")
+//        previousTotalSteps = savedNumber
+//    }
+//
+//    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+//
+//    }
+
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment HomeFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            HomeFragment().apply {
+//                arguments = Bundle().apply {
+//                }
+//            }
+//    }
+
 }
 
--- app/src/main/java/com/example/fitfiesta/WorkoutProgressFragment.kt
+++ app/src/main/java/com/example/fitfiesta/WorkoutProgressFragment.kt
@@ -5,27 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [WorkoutProgressFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class WorkoutProgressFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    lateinit var stepsProgressBar: CircularProgressBar
+    lateinit var caloriesProgressBar: CircularProgressBar
+    lateinit var stepsCount: TextView
+    lateinit var caloriesCount: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -34,26 +28,24 @@ class WorkoutProgressFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_workout_progress, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_workout_progress, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WorkoutProgressFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WorkoutProgressFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        stepsProgressBar = view.findViewById(R.id.stepsProgress)
+        stepsCount = view.findViewById(R.id.stepsCount)
+        caloriesProgressBar = view.findViewById(R.id.caloriesProgress)
+        caloriesCount = view.findViewById(R.id.caloriesCount)
+
+        var steps = stepsCount.toString()
+        var calories = caloriesCount.toString()
+
+        stepsProgressBar.apply {
+            setProgressWithAnimation(steps.toFloat())
+        }
+
+        caloriesProgressBar.apply {
+            setProgressWithAnimation(calories.toFloat())
+        }
+
+        return view
     }
 }
